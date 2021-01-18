af1afff738652690ba10be167ad2a0a74449be66
yellowei <me@yellowei.com>
2021-01-18 11:33:57 +0800

    Update production.js

--- config/environment/production.js
+++ config/environment/production.js
@@ -1,16 +1,21 @@
 process.env.HOST = 'http://localhost:8089/';
 process.env.TRACKER_HOST = 'http://localhost:8088';
-process.env.MAIN_DB_HOST = '192.168.31.170';
 process.env.MAIN_DB_NAME = 'mainDB';
-process.env.DB_PORT = 27017;
+// process.env.MAIN_DB_HOST = 'localhost';
+// process.env.DB_PORT = 27017;
+process.env.MAIN_DB_HOST = 'ez70ha696r.54http.tech';
+process.env.DB_PORT = 55663;
 process.env.DB_USER = 'admin';
 process.env.DB_PASS = '1q2w3e!@#';
 process.env.OXR_APP_ID = '451784939cd549259b92764b7c4eced3';
 
+
 process.env.SERVER_TYPE = 'openSource';
 process.env.SERVER_PLATFORM = 'openSource';
-process.env.SOCKET_DB_PORT = 6379;
-process.env.SOCKET_DB_HOST = '192.168.31.170';
+//process.env.SOCKET_DB_PORT = 6379;
+//process.env.SOCKET_DB_HOST = 'localhost';
+process.env.SOCKET_DB_PORT = 52515;
+process.env.SOCKET_DB_HOST = 'eeweoqyuv2.54http.tech';
 process.env.SOCKET_DB = 9;
 
 
